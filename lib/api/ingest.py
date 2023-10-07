579fa517aee2738809b1decc0d4487de7c16c21f
Ismail Pelaseyed <homanp@gmail.com>
2023-10-07 22:40:09 +0200

    Add embedding and finetune flows

--- lib/api/ingest.py
+++ lib/api/ingest.py
@@ -1,9 +1,7 @@
-import openai
 from fastapi import APIRouter
 
 from lib.models.ingest import IngestRequest
-from lib.service.embedding import EmbeddingService
-from lib.service.finetune import get_finetuning_service
+from service.flows import create_finetune, create_embeddings
 from lib.utils.prisma import prisma
 
 router = APIRouter()
@@ -16,21 +14,7 @@ router = APIRouter()
 )
 async def ingest(body: IngestRequest):
     """Endpoint for ingesting data"""
-    webhook_url = body.webhook_url
     datasource = await prisma.datasource.create(data={**body})
-    embedding_service = EmbeddingService(datasource=datasource)
-    documents = await embedding_service.generate_documents()
-    nodes = await embedding_service.generate_chunks(documents=documents)
-    await embedding_service.generate_embeddings(nodes=nodes)
-    finetunning_service = await get_finetuning_service(
-        nodes=nodes, provider="openai", batch_size=5
-    )
-    await finetunning_service.generate_dataset()
-    finetune_job = await finetunning_service.finetune()
-    finetune = await openai.FineTune.retrieve(id=finetune_job.id)
-    await prisma.datasource.update(
-        where={"id": datasource.id},
-        data={"webhook_url": webhook_url, "finetune": finetune},
-    )
-    await finetunning_service.cleanup(training_file=finetune_job.get("training_file"))
+    await create_embeddings(datasource=datasource)
+    await create_finetune(datasource=datasource)
     return {"success": True, "data": datasource}
